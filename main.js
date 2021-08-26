c4f946d3a695660bf3f1a39d66d346e16aff1b69
nazima4 <nazimaabdyrahmanova4@gmail.com>
2021-08-26 14:13:12 +0300

    initial

--- main.js
+++ main.js
@@ -14,3 +14,14 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+//sss
+const burgerMenu = document.querySelector('.mobile-menu')
+const topMenu = document.querySelector('.top-menu')
+burgerMenu.addEventListener('click', () => {
+    topMenu.classList.toggle('show')
+})
+// Hide menu  when click nav links
+const  menulinks = document.querySelectorAll('.top-menu a')
+menulinks.forEach(item => item.addEventListener('click', () => {
+    topMenu.classList.remove('show')
+}))
